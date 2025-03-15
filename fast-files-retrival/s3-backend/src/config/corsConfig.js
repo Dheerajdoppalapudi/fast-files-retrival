5c23b885ab2cda03a976ab855ec17854ef6ca0c7
ankit435 <ankitkumarsingh435@gmail.com>
2025-03-15 21:13:45 +0530

    firstcommit

--- fast-files-retrival/s3-backend/src/config/corsConfig.js
+++
@@ -1,8 +0,0 @@
-const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
-
-export default corsOptions;
-
